10b5622da64340efbd4084f2393afe5ac6b2ef2a
Marty Rath <91559109+MartyRath@users.noreply.github.com>
2023-06-23 17:16:08 +0100

    add reading almost done

---
+++ controllers/station-controller.js
@@ -0,0 +1,26 @@
+import { stationStore } from "../models/station-store.js";
+import { readingStore } from "../models/reading-store.js";
+
+export const stationController = {
+  async index(request, response) {
+    const station = await 
+    stationStore.getStationById(request.params.id);
+    const viewData = {
+      name: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+
+  async addReading(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReading = {
+      title: request.body.title,
+      artist: request.body.artist,
+      duration: Number(request.body.duration),
+    };
+    console.log(`adding reading ${newReading.code}`);
+    await readingStore.addReading(station._id, newReading);
+    response.redirect("/station/" + station._id);
+  },
+};
