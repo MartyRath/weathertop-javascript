bf57a5e79b9696f39c3a2b53c970e6983ab39cb6
Marty Rath <91559109+MartyRath@users.noreply.github.com>
2023-07-16 13:20:37 +0100

    Trends added

--- utils/station-analytics.js
+++ utils/station-analytics.js
@@ -12,12 +12,12 @@ export const stationAnalytics = {
     getMaxValue(readings, property) {
         let maxValue = Number.MIN_SAFE_INTEGER;
         for (let reading of readings) {
-          if (reading[property] > maxValue) {
-            maxValue = reading[property];
-          }
+            if (reading[property] > maxValue) {
+                maxValue = reading[property];
+            }
         }
         return maxValue;
-      },
+    },
 
     getMinValue(readings, property) {
         let minValue = Number.MAX_SAFE_INTEGER;
@@ -28,5 +28,26 @@ export const stationAnalytics = {
         }
         return minValue;
     },
-    
+
+    getTrends(readings, property) {
+        if (readings.length < 3) {
+            return null;
+        }
+
+        const lastReading = readings[readings.length - 1];
+        const secondLastReading = readings[readings.length - 2];
+        const thirdLastReading = readings[readings.length - 3];
+
+        const last = lastReading[property];
+        const secondLast = secondLastReading[property];
+        const thirdLast = thirdLastReading[property];
+
+        if (last > secondLast && secondLast > thirdLast) {
+            return "bxs:up-arrow";  //rising arrow
+        } else if (last < secondLast && secondLast < thirdLast) {
+            return "bxs:down-arrow"; //falling arrow
+        }
+        return null;
+    },
+
 };
